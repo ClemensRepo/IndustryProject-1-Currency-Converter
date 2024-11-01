394f08d0bf44bb689589d4386e46ba0e41ecb256
Clemens Lee <operations.sg65@outlook.com>
2024-11-01 15:56:05 +0800

    Form, chart, table and map skeleton html ready. Leaflet JS tempalte ready

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 
 function main() {
-    let currencyData = [];
+    
+    init();
+    let currencyChoice = [];
     window.addEventListener("DOMContentLoaded", async() =>{
         currencyData = await loadData();
-    })
+    });
+
+    function init() {
+        let singapore = [1.35,103.82];
+        let singaporeMap = L.map("map");
+        singaporeMap.setView(singapore, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    }
 }
 
 main();
